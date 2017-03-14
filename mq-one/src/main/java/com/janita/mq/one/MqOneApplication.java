ef793069cec64dcd73ae571ba18835d1e1fad7b8
Janita <zhuchenjian@huizhi-tech.com>
2017-03-14 19:51:45 +0800

    两个项目启动类

---
+++ mq-one/src/main/java/com/janita/mq/one/MqOneApplication.java
@@ -0,0 +1,14 @@
+package com.janita.mq.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/3/14
+ */
+@SpringBootApplication
+public class MqOneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqOneApplication.class,args);
+    }
+}
